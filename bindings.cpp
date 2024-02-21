6735c91882f1313586fc2afdde2fef105b0b1b0c
lvk88 <15655519+lvk88@users.noreply.github.com>
2024-02-21 22:48:40 +0100

    Add constructor that takes width height and image data

--- bindings.cpp
+++ bindings.cpp
@@ -1,11 +1,25 @@
 #include "lib.hpp"
 
+#include <cstdint>
 #include <emscripten/bind.h>
+#include <stdexcept>
+#include <iostream>
+
+lvk88::arc::SizedSingleChannelImage* createSizedSingleImageFromJSArray(int width, int height, emscripten::val js_data)
+{
+  std::vector<std::uint8_t> data = emscripten::vecFromJSArray<std::uint8_t>(js_data);
+  lvk88::arc::SizedSingleChannelImage* result = new lvk88::arc::SizedSingleChannelImage();
+  result->width = width;
+  result->height = height;
+  result->data = data;
+  return result;
+}
 
 EMSCRIPTEN_BINDINGS(emarclib)
 {
   emscripten::class_<lvk88::arc::SizedSingleChannelImage>("SizedSingleChannelImage")
     .constructor<>()
+    .constructor(&createSizedSingleImageFromJSArray)
     .property("width", &lvk88::arc::SizedSingleChannelImage::width)
     .property("height", &lvk88::arc::SizedSingleChannelImage::height)
     .property("data", &lvk88::arc::SizedSingleChannelImage::data);
