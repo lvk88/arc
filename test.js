ab361ce3f59fb8579a4d93cc3c42f82301283520
lvk88 <15655519+lvk88@users.noreply.github.com>
2024-02-21 22:48:56 +0100

    Add 'test' driver from node side

---
+++ test.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+async function main() {
+  const module = await require("./emarclib")();
+  try {
+    const buffer = fs.readFileSync('out.lvk88')
+
+    const magicNumber = buffer.readUint8(0);
+    console.log("Read magic number ", magicNumber);
+
+    const width = buffer.readInt32LE(1);
+    const height = buffer.readInt32LE(5);
+    console.log("w x h: ", width, " x " , height);
+
+    const pixelData = buffer.slice(9);
+    const pixelArray = Uint8Array.from(pixelData);
+    console.log(pixelArray)
+
+    console.log("Value of first pixel: ", pixelArray[0]);
+
+    const img = new module.SizedSingleChannelImage(width, height, pixelArray);
+
+    module.mesh_image(img)
+
+  } catch (error) {
+    console.log('Error readin file: ', error);
+  }
+}
+
+main();
