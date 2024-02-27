91c116784390a0607a229cb4dba4b01fa7b5130c
lvk88 <15655519+lvk88@users.noreply.github.com>
2024-02-27 19:53:17 +0100

    Draw nodes of resulting mesh

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,6 +1,9 @@
 import Module from "emarclib";
 import { MainModule } from "emarclib-types";
 
+const canvas = <HTMLCanvasElement>document.getElementById("postproc-area");
+const ctx = canvas.getContext("2d");
+
 const readBufferData = (buffer: Uint8Array) => {
   const magicNumber = buffer[0];
   const width = new DataView(buffer.buffer).getUint32(1, true);
@@ -28,6 +31,18 @@ const main = async () => {
   const img = new m.SizedSingleChannelImage(data.width, data.height, data.pixelData);
 
   const res = m.mesh_image(img);
+
+  // Note: I keep getting OOM errors if I try to
+  // use nodeCoordinates through res, e.g.:
+  // const x = res.nodeCoordinates.get(i)
+  const nodeCoordinates = res.nodeCoordinates;
+
+  for(var i = 0; i < nodeCoordinates.size(); i += 3){
+    const x = nodeCoordinates.get(i);
+    const y = nodeCoordinates.get(i + 1);
+
+    ctx.fillRect(x, 200 - y, 1, 1);
+  }
 }
 
 main();
